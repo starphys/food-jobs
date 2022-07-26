ff1adaefc2cf1aa4a1d68deb2d68ca36ca686dc0
Evan Astle <starphys@gmail.com>
2022-07-25 20:54:06 -0700

    Moved listings data to separate file (readability)

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,28 +1,7 @@
 import {GoogleMap, useLoadScript} from "@react-google-maps/api";
 import Markers from './Markers';
 import configInfo from "../config.json"
-
-const listings = [{
-  id: 1,
-  title: "Host/Hostess",
-  company: "Chunky Cheese",
-  pay: 15,
-  position: { lat: 37.33493148426, lng: -121.8808998386},
-},
-{
-  id: 2,
-  title: "Waiter",
-  company: "Chunkly Cheese",
-  pay: 15,
-  position: { lat: 37.33593148426, lng: -121.8818998386},
-},
-{
-  id: 3,
-  title: "Mouse",
-  company: "Chunkly Cheeks",
-  pay: 15,
-  position: { lat: 37.33693148426, lng: -121.8828998386},
-},]
+import {listings} from "../data/listings"
 
 const defaultMapContainerStyle = {
   width: "100vw",
