bf61e6a8c87e5b2b96cbbcbf2419f8ba94ff497b
Evan Astle <starphys@gmail.com>
2022-07-25 20:50:02 -0700

    Working with multiple pins simultaneously

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,14 +1,28 @@
 import {GoogleMap, useLoadScript} from "@react-google-maps/api";
-import MarkerComponent from './MarkerComponent';
+import Markers from './Markers';
 import configInfo from "../config.json"
 
-const listing = {
-  id: 3,
+const listings = [{
+  id: 1,
   title: "Host/Hostess",
   company: "Chunky Cheese",
   pay: 15,
   position: { lat: 37.33493148426, lng: -121.8808998386},
-}
+},
+{
+  id: 2,
+  title: "Waiter",
+  company: "Chunkly Cheese",
+  pay: 15,
+  position: { lat: 37.33593148426, lng: -121.8818998386},
+},
+{
+  id: 3,
+  title: "Mouse",
+  company: "Chunkly Cheeks",
+  pay: 15,
+  position: { lat: 37.33693148426, lng: -121.8828998386},
+},]
 
 const defaultMapContainerStyle = {
   width: "100vw",
@@ -36,7 +50,7 @@ function MapComponent({mapContainerStyle = defaultMapContainerStyle, center = de
       mapContainerStyle={mapContainerStyle}
       zoom={zoom}
       center={center}>
-      <MarkerComponent listing={listing} />
+      <Markers listings={listings} />
     </GoogleMap>
   </div> );
 }
