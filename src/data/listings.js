ce8463ede0dd9990457f4a7d314e7574e2cb6110
skytoad <t.froggy@gmail.com>
2022-07-28 21:14:19 -0700

    26 job listings/markers

--- src/data/listings.js
+++ src/data/listings.js
@@ -1,19 +1,213 @@
 export const listings = [
-  {
-      id: 1,
-      title: "Whispers Cafe and Creperie",
-      pos: "Server",
-      desc: "Become a server and join our amazing team",
-      pay: "$17.50 + tips",
-      position: { lat: 37.33392966859074, lng: -121.88669260386027}
-  },
-  {
-      id: 2,
-      title: "Paper Plane",
-      pos:  "Bartender",
-      desc: "Serve artisinal drinks at this fine establishment",
-      pay: "$18.00 + tips",
-      position: { lat: 37.33504654119588, lng: -121.8894964369294}
-     
-  }
+    {
+        id: 1,
+        title: "Whispers Cafe and Creperie",
+        pos: "Server",
+        desc: "Become a server and join our amazing team.",
+        pay: "$17.50 + tips",
+        position: { lat: 37.33392966859074, lng: -121.88669260386027 }
+    },
+    {
+        id: 2,
+        title: "Paper Plane",
+        pos:  "Bartender",
+        desc: "Serve artisanal drinks at this fine establishment.",
+        pay: "$18.00 + tips",
+        position: { lat: 37.33504654119588, lng: -121.8894964369294 }
+    },
+    {
+        id: 3,
+        title: "SJSU Dining Commons",
+        pos: "Server",
+        desc: "Support SJSU students with nutritious and delicious meals",
+        pay: "$18.00",
+        position: { lat: 37.334150870352104, lng: -121.87863608270348 }
+    },
+    {
+        id: 4,
+        title: "Sammy G's Pizzeria",
+        pos:  "Bartender",
+        desc: "Serve beer and keep the customers happy",
+        pay: "$18.00 + tips",
+        position: { lat: 37.335856934092185, lng: -121.87706956124886 } 
+    },
+    {
+        id: 5,
+        title: "House Of Bagels",
+        pos: "Sandwich Maker",
+        desc: "Handle customer sales and build bagel sandwiches",
+        pay: "$25.00",
+        position: { lat: 37.336791052257446, lng: -121.87635075120511 }
+    },
+    {
+        id: 6,
+        title: "Peanuts Deluxe Cafe",
+        pos:  "Waiter",
+        desc: "Avoid the fuss and deliver delicious diner food to customers",
+        pay: "$16.00",
+        position: { lat: 37.33743087872057, lng: -121.88354985176741 }
+       
+    },
+    {
+        id: 7,
+        title: "My Milkshake",
+        pos: "Milkshake Master",
+        desc: "Mix shakes and fulfill all business needs",
+        pay: "$18.00",
+        position: { lat: 37.33429165813058, lng: -121.8878306481656 }
+    },
+    {
+        id: 8,
+        title: "La Victoria Taqueria",
+        pos:  "Server",
+        desc: "Wait tables, deliver food, and ensure cleanliness in entire establishment",
+        pay: "$13.00 + tips",
+        position: { lat: 37.33268790677408, lng: -121.8844832628697 }
+    },
+    {
+        id: 9,
+        title: "TK Noodle #1",
+        pos:  "Server",
+        desc: "Provide direct food services to hungry customers",
+        pay: "$19.00",
+        position: { lat: 37.33156183853915, lng: -121.87925833746608 } 
+    },
+    {
+        id: 10,
+        title: "Okayama Sushi",
+        pos: "Waiter",
+        desc: "Keep restaurant clean and serve drinks and food to customers",
+        pay: "$27.50 + tips",
+        position: { lat: 37.34861996740769, lng: -121.89286885207291 }
+    },
+    {
+        id: 11,
+        title: "San Pedro Square Market",
+        pos:  "Bartender",
+        desc: "Serve drinks to variety of clients",
+        pay: "$24",
+        position: { lat: 37.336688374243735, lng: -121.8943606244857 }
+       
+    },
+    {
+        id: 12,
+        title: "Mezcal",
+        pos: "Waiter",
+        desc: "Deliver delicious plates to tables, take orders, and handle customer concerns",
+        pay: "$18.00",
+        position: { lat: 37.334150870352104, lng: -121.87863608270348 }
+    },
+    {
+        id: 13,
+        title: "Ludwig's German Table",
+        pos:  "Bartender",
+        desc: "Assist customers in enjoying craft beers and artisanal dinners at this vintage German beer garden",
+        pay: "$22.00",
+        position: { lat: 37.33669302015019, lng: -121.88116804089186 }
+    },
+    {
+        id: 14,
+        title: "Swaad Indian Cuisine",
+        pos:  "Server",
+        desc: "Provide direct food services to hungry customers",
+        pay: "$18.00 + tips",
+        position: { lat: 37.35122137823696, lng: -121.88424674226108 } 
+    },
+    {
+        id: 15,
+        title: "Vegetarian House",
+        pos:  "Server",
+        desc: "Provide direct food services to hungry customers",
+        pay: "$18.00 + tips",
+        position: { lat: 37.33669302015019, lng: -121.88116804089186 } 
+    },
+    {
+        id: 16,
+        title: "Casa Vicky",
+        pos:  "Server",
+        desc: "Serve customer needs and occassionaly assist in dishwashing.",
+        pay: "$20.00 + tips",
+        position: { lat: 37.34831751956042, lng: -121.8778373077169 } 
+    },
+    {
+        id: 17,
+        title: "Cali Spartan",
+        pos:  "Waiter",
+        desc: "Clear tables, run food, make customers happy.",
+        pay: "$14.00",
+        position: { lat: 37.33288168132078, lng: -121.87544889430258 } 
+    },
+    {
+        id: 18,
+        title: "La Peñita",
+        pos:  "Server/Host",
+        desc: "Greet and seat customers, take orders, and deliver delicious meals to tables.",
+        pay: "$18.00 + tips",
+        position: { lat: 37.32747392939281, lng: -121.88360487068437 } 
+    },
+    {
+        id: 19,
+        title: "Original Joe's",
+        pos:  "Server",
+        desc: "Provide direct food services to hungry customers",
+        pay: "$12.00 + tips",
+        position: { lat: 37.33669302015019, lng: -121.88116804089186 } 
+    },
+    {
+        id: 20,
+        title: "Angelou's Mexican Grill",
+        pos:  "Server",
+        desc: "Wait tables, deliver food, and ensure cleanliness in entire establishment.",
+        pay: "$18.00 + tips",
+        position: { lat: 37.33767616919154, lng: -121.89021323979503 } 
+    },
+    {
+        id: 21,
+        title: "La Victoria Taqueria",
+        pos:  "Server",
+        desc: "Wait tables, deliver food, and ensure cleanliness in entire establishment.",
+        pay: "$13.00 + tips",
+        position: { lat: 37.34178121193711, lng: -121.89079663771749 }
+    },
+    {
+        id: 22,
+        title: "La Victoria Taqueria",
+        pos:  "Server",
+        desc: "Wait tables, deliver food, and ensure cleanliness in entire establishment",
+        pay: "$19.00 + tips",
+        position: { lat: 37.32433094261951, lng: -121.88575895166865 }
+    },
+    {
+        id: 23,
+        title: "Bún Bò Huế An Nam",
+        pos:  "Waiter",
+        desc: "Satisfy customers by ensuring complete happiness with our delicious pho bowls and bun bo hue.",
+        pay: "$13.00 + tips",
+        position: { lat: 37.3288999262719, lng: -121.85838706476999 }
+    },
+    {
+        id: 24,
+        title: "Palermo Italian",
+        pos:  "Waiter",
+        desc: "Serve spaghetti, meatballs, wine, and cheeses to tables of hungry customers.",
+        pay: "$24.00 + tips",
+        position: { lat: 37.32190405996976, lng: -121.90218165114642 }
+    },
+    {
+        id: 25,
+        title: "Cafe Roselena",
+        pos:  "Server",
+        desc: "Do server things and have a good time.",
+        pay: "$14.00 + tips",
+        position: { lat: 37.32190405996976, lng: -121.90218165114642 }
+    },
+    {
+        id: 26,
+        title: "Panda Express",
+        pos:  "Server",
+        desc: "Provide direct food services to hungry customers",
+        pay: "$18.00 + tips",
+        position: { lat: 37.33669302015019, lng: -121.88116804089186 }
+    },
 ]
+
