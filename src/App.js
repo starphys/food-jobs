403d6a774179d90339d9d7db1ed0cae957837d6f
Sahiti Hibane <sahitihibane@Sahitis-MacBook-Air.local>
2022-07-31 19:50:59 -0700

    add profile page link

--- src/App.js
+++ src/App.js
@@ -1,70 +1,55 @@
 import './App.css';
-import MapComponent from './components/MapComponent';
-import {listings} from "./data/listings"
-import Navbar from './Navbar';
-import Aboutpage from './pages/Aboutpage';
+// import MapComponent from './components/MapComponent';
+// import {listings} from "./data/listings"
+import Navbar from "./Navbar";
+import Aboutpage from "./pages/Aboutpage";
 import Contactpage from './pages/Contactpage';
 import Homepage from './pages/Homepage';
 import Joblistingspage from './pages/Joblistingspage';
 import Profilepage from './pages/Profilepage';
 import Relaxpage from './pages/Relaxpage';
 import Skillspage from './pages/Skillspage';
+// to bring all the routes in one place:
+import {Route, Routes} from "react-router-dom"
 
-const mapContainerStyle = {
-  width: "100vw",
-  height: "100vh",
-};
+// const mapContainerStyle = {
+//   width: "100vw",
+//   height: "100vh",
+// };
 
-const center = {
-  lat: 37.3352,
-  lng: -121.8811,
-}
+// const center = {
+//   lat: 37.3352,
+//   lng: -121.8811,
+// }
 
-const mapOptions = {
-  disableDefaultUI : true,
-  styles : [ 
-    {
-      featureType: "poi",
-      elementType: "all",
-      stylers: [ { "visibility": "off" } ]
-    }
-  ]
-}
+// const mapOptions = {
+//   disableDefaultUI : true,
+//   styles : [ 
+//     {
+//       featureType: "poi",
+//       elementType: "all",
+//       stylers: [ { "visibility": "off" } ]
+//     }
+//   ]
+// }
 
 function App() {
-  let DisplayPage
-  switch (window.location.pathname) {
-    case "/":
-      DisplayPage = App
-      break
-    case "/about":
-      DisplayPage = Aboutpage
-      break
-    case "/joblistings":
-      DisplayPage = Joblistingspage
-      break
-    case "/skills":
-      DisplayPage = Skillspage
-      break
-    case "/relax":
-      DisplayPage = Relaxpage
-      break
-    case "/profile":
-      DisplayPage = Profilepage
-      break
-    case "/contact":
-      DisplayPage = Contactpage
-      break 
-  }
-
   return (
-    <div> 
     <>
-      {/* <MapComponent mapContainerStyle={mapContainerStyle} center={center} zoom={15} listings={listings} options={mapOptions}/> */}
       <Navbar />
-      <DisplayPage />
+      <div className="box">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<Aboutpage />} />
+          <Route path="/joblistings" element={<Joblistingspage />} />
+          <Route path="/skills" element={<Skillspage />} />
+          <Route path="/relax" element={<Relaxpage />} />
+          <Route path="/profile" element={<Profilepage />} />
+          <Route path="/contact" element={<Contactpage />} />
+        </Routes>
+        </div>
+      
     </>
-    </div>
   )
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,27 +1,24 @@
+import { Link } from "react-router-dom" // works as anchor tag
 export default function Navbar() {
     return (
         <nav className="navigation">
-            <a href="/" className="website-name">Spatula <span role="img" aria-label="fork-and-knife">🍽️</span></a>
+            <Link to="/" className="website-name">Spatula <span role="img" aria-label="fork-and-knife">🍽️</span></Link>
             <ul>
-                <li > 
-                    <a href="/about">About Us</a>
-                </li>
-                <li>
-                    <a href="/joblistings">Map Job Listings</a>
-                </li>
-                <li>
-                    <a href="/skills">SkillBuild</a>
-                </li>
-                <li>    
-                    <a href="/relax">Relax</a>
-                </li>
-                <li>
-                    <a href="/profile">Profile</a>
-                </li>
-                <li>
-                    <a href="/contact">Contact Us</a>
-                </li>
+                <displayPage to="/about">About Us</displayPage>
+                <displayPage to="/joblistings">SkillBuild</displayPage>
+                <displayPage to="/skills">Relax</displayPage>
+                <displayPage to="/profile">Profile</displayPage>
+                <displayPage to="/contact">Contact Us</displayPage>
             </ul>
         </nav>
+    )
+}
+
+function displayPage({to, children,...props}) {
+    const path = window.location.pathname
+    return (
+        <li className={path === to ? "active" : ""}>
+            <Link to={to} {...props}>{children}</Link>
+        </li>
     )
 }
