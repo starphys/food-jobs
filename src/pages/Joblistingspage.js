c743b8307f309582f8f5e5a6257fca6edc2e4ea3
Sahiti Hibane <sahitihibane@Sahitis-MacBook-Air.local>
2022-08-01 22:20:39 -0700

    add map

--- src/pages/Joblistingspage.js
+++ src/pages/Joblistingspage.js
@@ -1,3 +1,27 @@
+import MapComponent from './components/MapComponent';
+import {listings} from "./data/listings"
+
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+
+const center = {
+  lat: 37.3352,
+  lng: -121.8811,
+}
+
+const mapOptions = {
+  disableDefaultUI : true,
+  styles : [ 
+    {
+      featureType: "poi",
+      elementType: "all",
+      stylers: [ { "visibility": "off" } ]
+    }
+  ]
+}
+
 export default function Joblistingspage() {
     return <h1>Job Listings</h1>
 }
