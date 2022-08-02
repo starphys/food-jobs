cba5c714b724eac0a3f851bb5e8c6a40d05da85c
Sahiti Hibane <sahitihibane@Sahitis-MacBook-Air.local>
2022-08-02 11:03:23 -0700

    update job listings page

--- src/pages/Joblistingspage.js
+++ src/pages/Joblistingspage.js
@@ -25,6 +25,18 @@ const mapOptions = {
 export default function Joblistingspage() {
     return (<>
         <h1>Job Listings</h1>
+        <h2>
+          Reduce your application process time by 10x with just a click!
+        </h2>
+        <p>
+          From the below map, click on any of the red job markers. 
+          Once you find a job you like, 
+          just submit with the "apply" button, and you are done!
+        </p>
+        <p>
+          PRO TIP: For convenience, you can consider jobs that are close to transit stations that are shown as blue transit icons.
+          You can click on those icons to see find more information about the stations.
+        </p>
         <div>
             <MapComponent mapContainerStyle={mapContainerStyle} center={center} zoom={15} listings={listings} options={mapOptions} />
         </div>
